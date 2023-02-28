38a61d51d11547daa87b4648bba7f48f2afb41c4
SHERAZSHAHZAD <119057763+SHERAZSHAHZAD@users.noreply.github.com>
2023-02-28 09:32:58 -0800

    Js Assignment 10

---
+++ script.js
@@ -0,0 +1,110 @@
+// task no 3
+c=2
+d=1
+
+// tack no 4
+awser = 5;
+
+// task no 5
+let a = parseInt(prompt("First number?",1));
+let b = parseInt(prompt("Second number?",2));
+
+alert(a + b);
+
+// task no 6
+
+(a + b < 4) ? "Below" :  "over";
+result == over;
+
+// task no 7 copy sir tenry oprater
+
+// if (login == 'Employee') {
+//     message = 'Hello';
+//   } else if (login == 'Director') {
+//     message = 'Greetings';
+//   } else if (login == '') {
+//     message = 'No login';
+//   } else {
+//     message = '';
+//   }
+
+//   convert in tenery oparter
+
+let login = prompt("show status","");
+(login == "Employee") ? alert("Hello") :
+(login == "Director") ? alert("Greetings") :
+(login == "") ? alert("NO login") : alert("Invalid status");
+
+// task no 8
+alert( null || 2 || undefined );
+output = 2
+
+// in the case the operator check each statment.the statment nor equal ((anything that is not null, undefined, 0, false, or an empty string)) the operator check first statment if statment is equal uper cases the operator check statment.the statment is real operator print real statment.operator check statment (left to right)  
+
+// task no 9
+
+alert( alert(1) || 2 || alert(3) );
+output = 1, 2; 
+
+// task no 10
+
+alert( 1 && null && 2 );
+output = null
+
+// in the case the operator check different statments if first statment is correct the operator check the next  statment if scend statment is not correct the operator does not check next statment.the operator print scend statment. operator check statment (right to left)
+
+// task no 11
+
+alert( alert(1) && alert(2) );
+output = undefined;
+
+// in the case the innner statment show first alert(first alert value is 1) first alert is undefined the secend operator check secend alert.show scend alert(scend alert value is 2) scend alert is undefind.
+
+// task no 12
+
+alert( null || 2 && 3 || 4 );
+output = 3;
+
+// in the case This expression uses the logical OR operator (||) and the logical AND operator (&&) the ((&&) operator values checked (right to left).the operator check first value.first value is corret.the operator check next value the scend value is corret.the operator print first value.
+// secend case the operator check value (left to right).the operator check first value 
+
+// task no 02
+
+5>4;
+output=true;
+// the equcation is true.
+
+"apple" > "pineapple"
+output = false;
+
+undefined == null
+output = true;
+// in the case the undefine has empty and null have empty."undefined" is used whily the variable is not decleare.and "null" is used when the variable value not decleare. 
+
+undefined === null
+output = false
+// When using the strict equality operator ===, JavaScript checks both the value and the type of the operands. Since "undefined" and "null" have different types, the expression undefined === null returns false.
+
+// task no 13
+
+let age = Number.parseInt(prompt("Enter your age"));
+// if(age>=14 && age <=90){
+//     console.log("Age is between 14 and 90")
+// }
+// else{
+//     console.log("The Age is not between 14 and 90")
+// }
+
+// using not opperator
+
+if (!(age>=14 && age <=90)){
+    console.log("The age is not between 14 and 90")
+
+}
+
+else{
+    console.log("Age is between 14 and 90")
+}
+
+// task no 14
+
